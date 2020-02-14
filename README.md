6fe8fa0d32f178eaa593f8f5580d8dc5fcb45b1d
adityakalra581 <adityakalra581@gmail.com>
2020-02-14 16:15:19 +0530

    Push Button Update

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ This is an introductory Project on RPi and Web Server.
 
 The Code is referenced from the article:
 Link: https://randomnerdtutorials.com/raspberry-pi-web-server-using-flask-to-control-gpios/
+
+Soon will integrate this led code with push button to control led working with this button.
+LINK: https://youtu.be/LEi_dT9KDJI
---
+++ button.py
@@ -0,0 +1,37 @@
+
+## This code is a basic working of a push button.
+## When the button is pressed LED gets on and as soon as
+## button is released led get's off.
+
+## Also the led is by default off.
+
+
+
+
+
+
+
+
+
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+
+sleepTime= .1
+
+led=4
+button=17
+
+
+GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(led, GPIO.OUT)  
+GPIO.output(led,False)     ## LED will remain off by-default.
+
+try:
+    while True:
+        GPIO.output(led, not GPIO.input(button))
+        sleep(.1)
+finally:
+    GPIO.output(led,False)
+    GPIO.cleanup()
